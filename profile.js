94505c19be917f58a0f6782c8415f4625d434b7c
Your Name <Philip Essien>
2023-12-18 23:11:02 +0000

    first commit

--- profile.js
+++ profile.js
@@ -5,7 +5,8 @@ function ProfilePage() {
   return (
     <div>
         <NavBar />
-      <h1>All About Me!</h1>
+      <h1 >All about Arsenal Football Club</h1>
+      <p>Why they are the best team in the world</p>
       <p>I like movies and blah blah blah blah blah</p>
       <img src="https://content.codecademy.com/courses/React/react_photo-monkeyselfie.jpg" />
     </div>
